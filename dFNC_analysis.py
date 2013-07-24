5ef95e029799882eca90f1b5259082ec46396cc1
Jeremy Elman <jelman@lbl.gov>
2013-07-24 15:32:51 -0700

    include code to run group analysis with randomise

--- dFNC_analysis.py
+++ dFNC_analysis.py
@@ -5,19 +5,20 @@ from glob import glob
 import numpy as np
 
     
-#### Set parameters############
-datadir = '/home/jagust/rsfmri_ica/GIFT/GICA_d40/dFNC'
+####################### Set parameters################
+datadir = '/home/jagust/rsfmri_ica/GIFT/GICA_d30/dFNC'
 dfnc_info = 'rsfmri_dfnc.mat'
 globstr = '*_results.mat'
-dyn_mean_out = 'dFNC_dyn_mean.csv'
-dyn_std_out = 'dFNC_dyn_std.csv'
-spectra_mean_out = 'dFNC_spectra_mean.csv'
-spectra_std_out = 'dFNC_spectra_std.csv'
-dfnc_measures = ['FNCdyn', 'spectra_fnc']
+nnodes = 18
+dfnc_measures = {'corr':'FNCdyn', 'spectra':'spectra_fnc'}
 dfnc_stats = {'mean':np.mean, 'std':np.std}
-###############################
+modeldir = '/home/jagust/rsfmri_ica/GIFT/models'
+des_file = os.path.join(modeldir, 'PIB_Age_Scanner_Motion_GM.mat')
+con_file = os.path.join(modeldir, 'PIB_Age_Scanner_Motion_GM.con')
+dfnc_data_file = os.path.join(datadir, 'dFNC_corr_std.csv')
+##############################################################
 
-#Get analysis info  numbers
+#Get analysis info numbers
 info_file = os.path.join(datadir, dfnc_info)
 comp_nums, ncomps, nsubs = gift.dfnc_analysis_info(info_file)
 features = (ncomps*(ncomps-1))/2
@@ -25,7 +26,10 @@ features = (ncomps*(ncomps-1))/2
 # Get list of subject mat files
 sub_matfiles = glob(os.path.join(datadir, globstr))
 
-for measure in dfnc_measures:
+## Get data from dFNC output
+############################
+for measure_name in dfnc_measures.keys():
+    measure_field = dfnc_measures[measure_name]
     for stat_name in dfnc_stats.keys():
         stat_func = dfnc_stats[stat_name]
         allsub_array = np.zeros((nsubs, features))
@@ -33,7 +37,36 @@ for measure in dfnc_measures:
             subfile = sub_matfiles[i]
             sub_data = gift.get_dfnc_data(subfile, measure, stat_func)
             allsub_array[i] = sub_data
-            outname = '_'.join(['dFNC', measure, stat_name]) + '.csv'
+            outname = '_'.join(['dFNC', measure_name, stat_name]) + '.csv'
             outfile = os.path.join(datadir, outname)
             np.savetxt(outfile, allsub_array, delimiter='\t')
-            
+
+## Run group analysis with randomise
+####################################
+exists, resultsdir = gift.make_dir(datadir,
+                                    'randomise') 
+dfnc_data = np.genfromtxt(dfnc_data_file, names=None, dtype=float, delimiter=None)
+dfnc_img_fname = os.path.join(resultsdir, 'dFNC_std_4D.nii.gz')
+dfnc_saveimg = gift.save_img(dfnc_data, dfnc_img_fname)
+rand_basename = os.path.join(resultsdir, 'dFNC_std')
+p_uncorr_list, p_corr_list = gift.randomise(dfnc_saveimg, 
+                                            rand_basename, 
+                                            des_file, 
+                                            con_file)     
+uncorr_results = gift.get_results(p_uncorr_list)
+corr_results = gift.get_results(p_corr_list)
+       
+fdr_results = {}
+for i in range(len(uncorr_results.keys())):
+    conname = sorted(uncorr_results.keys())[i]
+    fdr_corr_arr = gift.multi_correct(uncorr_results[conname])
+    fdr_results[conname] = gift.square_from_combos(fdr_corr_arr, nnodes)
+    
+    outfile = os.path.join(resultsdir, 
+                        ''.join([rand_basename, '_fdr_corrp_','tstat',str(i+1),'.txt']))
+    # Save results to file
+    np.savetxt(outfile, 
+                fdr_results[conname], 
+                fmt='%1.5f', 
+                delimiter='\t')  
+    print('Saved corrected output to %s'%(outfile))        
