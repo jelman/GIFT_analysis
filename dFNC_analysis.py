157d319dd12a0eba0f7969fa74ad937d8cc50330
Jeremy Elman <jelman@lbl.gov>
2013-07-24 13:36:50 -0700

    Loops over dFNC measures and stats

--- dFNC_analysis.py
+++ dFNC_analysis.py
@@ -13,6 +13,8 @@ dyn_mean_out = 'dFNC_dyn_mean.csv'
 dyn_std_out = 'dFNC_dyn_std.csv'
 spectra_mean_out = 'dFNC_spectra_mean.csv'
 spectra_std_out = 'dFNC_spectra_std.csv'
+dfnc_measures = ['FNCdyn', 'spectra_fnc']
+dfnc_stats = {'mean':np.mean, 'std':np.std}
 ###############################
 
 #Get analysis info  numbers
@@ -23,26 +25,15 @@ features = (ncomps*(ncomps-1))/2
 # Get list of subject mat files
 sub_matfiles = glob(os.path.join(datadir, globstr))
 
-# Create empty arrays
-allsub_dyn_mean = np.zeros((nsubs, features))
-allsub_spectra_mean = np.zeros((nsubs, features))
-allsub_dyn_std = np.zeros((nsubs, features))
-allsub_spectra_std = np.zeros((nsubs, features))
-
-for i in range(len(sub_matfiles)):
-    subfile = sub_matfiles[i]
-    sub_dyn_mean, sub_dyn_std = gift.dfnc_dyn_stats(subfile)
-    sub_spectra_mean, sub_spectra_std = gift.dfnc_spectra_stats(subfile)
-    allsub_dyn_mean[i]=sub_dyn_mean   
-    allsub_spectra_mean[i]=sub_spectra_mean
-    allsub_dyn_std[i]=sub_dyn_std   
-    allsub_spectra_std[i]=sub_spectra_std
-    
-dyn_mean_outfile = os.path.join(datadir, dyn_mean_out)
-np.savetxt(dyn_mean_outfile, allsub_dyn_mean, delimiter='\t')
-dyn_std_outfile = os.path.join(datadir, dyn_std_out)
-np.savetxt(dyn_std_outfile, allsub_dyn_std, delimiter='\t')
-spectra_mean_outfile = os.path.join(datadir, spectra_mean_out)
-np.savetxt(spectra_mean_outfile, allsub_spectra_mean, delimiter='\t')
-spectra_std_outfile = os.path.join(datadir, spectra_std_out)
-np.savetxt(spectra_std_outfile, allsub_spectra_std, delimiter='\t')
+for measure in dfnc_measures:
+    for stat_name in dfnc_stats.keys():
+        stat_func = dfnc_stats[stat_name]
+        allsub_array = np.zeros((nsubs, features))
+        for i in range(len(sub_matfiles)):
+            subfile = sub_matfiles[i]
+            sub_data = gift.get_dfnc_data(subfile, measure, stat_func)
+            allsub_array[i] = sub_data
+            outname = '_'.join(['dFNC', measure, stat_name]) + '.csv'
+            outfile = os.path.join(datadir, outname)
+            np.savetxt(outfile, allsub_array, delimiter='\t')
+            
