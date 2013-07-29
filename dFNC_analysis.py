3b7f6427f052a530530a9cfb76224df6c03e9bcb
Jeremy Elman <jelman@lbl.gov>
2013-07-29 13:15:33 -0700

    Searches for dFNC result files and runs group analysis on each

--- dFNC_analysis.py
+++ dFNC_analysis.py
@@ -47,29 +47,32 @@ for measure_name in dfnc_measures.keys():
 ####################################
 exists, resultsdir = gift.make_dir(datadir,
                                     'randomise') 
-dfnc_data = np.genfromtxt(dfnc_data_file, names=None, dtype=float, delimiter=None)
-pth, fname, ext = gift.split_filename(dfnc_data_file)
-dfnc_img_fname = os.path.join(resultsdir, fname + '.nii.gz')
-dfnc_saveimg = gift.save_img(dfnc_data, dfnc_img_fname)
-rand_basename = os.path.join(resultsdir, fname)
-p_uncorr_list, p_corr_list = gift.randomise(dfnc_saveimg, 
-                                            rand_basename, 
-                                            des_file, 
-                                            con_file)     
-uncorr_results = gift.get_results(p_uncorr_list)
-corr_results = gift.get_results(p_corr_list)
-       
-fdr_results = {}
-for i in range(len(uncorr_results.keys())):
-    conname = sorted(uncorr_results.keys())[i]
-    fdr_corr_arr = gift.multi_correct(uncorr_results[conname])
-    fdr_results[conname] = gift.square_from_combos(fdr_corr_arr, nnodes)
-    
-    outfile = os.path.join(resultsdir, 
-                        ''.join([rand_basename, '_fdr_corrp_','tstat',str(i+1),'.txt']))
-    # Save results to file
-    np.savetxt(outfile, 
-                fdr_results[conname], 
-                fmt='%1.5f', 
-                delimiter='\t')  
-    print('Saved corrected output to %s'%(outfile))        
+resultsglob = os.path.join(datadir, 'dFNC_*.csv')
+result_files = glob(resultsglob)
+for dfnc_data_file in result_files:
+    dfnc_data = np.genfromtxt(dfnc_data_file, names=None, dtype=float, delimiter=None)
+    pth, fname, ext = gift.split_filename(dfnc_data_file)
+    dfnc_img_fname = os.path.join(resultsdir, fname + '.nii.gz')
+    dfnc_saveimg = gift.save_img(dfnc_data, dfnc_img_fname)
+    rand_basename = os.path.join(resultsdir, fname)
+    p_uncorr_list, p_corr_list = gift.randomise(dfnc_saveimg, 
+                                                rand_basename, 
+                                                des_file, 
+                                                con_file)     
+    uncorr_results = gift.get_results(p_uncorr_list)
+    corr_results = gift.get_results(p_corr_list)
+           
+    fdr_results = {}
+    for i in range(len(uncorr_results.keys())):
+        conname = sorted(uncorr_results.keys())[i]
+        fdr_corr_arr = gift.multi_correct(uncorr_results[conname])
+        fdr_results[conname] = gift.square_from_combos(fdr_corr_arr, nnodes)
+        
+        outfile = os.path.join(resultsdir, 
+                            ''.join([rand_basename, '_fdr_corrp_','tstat',str(i+1),'.txt']))
+        # Save results to file
+        np.savetxt(outfile, 
+                    fdr_results[conname], 
+                    fmt='%1.5f', 
+                    delimiter='\t')  
+        print('Saved corrected output to %s'%(outfile))        
