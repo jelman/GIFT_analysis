e04fed38c3b70d67bb10be034f6c1c1c1f9b3f96
Jeremy Elman <jelman@lbl.gov>
2013-08-16 15:45:26 -0700

    load subid mapping and add func to find subject file

--- regress_motion.py
+++ regress_motion.py
@@ -6,22 +6,27 @@ import general_utilities as utils
 import gift_utils as gu
 
 
-def backup_data(datadir, globstr):
+def backup_data(datadir, filelist):
     """
     back up all files in datadir matching pattern specified by globstr
     """
     backupdir = utils.make_dir(icadir, 'tc_backup')
-    tc_files = glob(os.path.join(icadir,tc_glob))
-    for src in tc_files:
+    for src in filelist:
         try:
             shutil.copy(src,backupdir)
         except:
             raise IOError('%s not copies'%src)
+            
+def find_sub_file(subid, filelist):
+    subfile = [x for x in filelist if subnum in x]
+    return subfile[0]    
     
-    
-    
-def main():
-    backup_data(datadir, tc_glob)    
+def main(icadir, datadir, tc_glob):
+    tc_files = glob(os.path.join(icadir,tc_glob))
+    backup_data(datadir, tc_files)  
+    for subid in sorted(subid_map.keys()):
+        subnum = subid_map[subid]
+        subtc = find_sub_file(subnum, tc_files)
     
 if __name__ == '__main__':
     
