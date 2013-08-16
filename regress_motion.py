a527fd2004bc01ba386c830fc9b7e7899499f85c
Jeremy Elman <jelman@lbl.gov>
2013-08-16 16:23:37 -0700

    regress out motion pars and save to nii

--- regress_motion.py
+++ regress_motion.py
@@ -18,10 +18,16 @@ def backup_data(datadir, filelist):
             raise IOError('%s not copies'%src)
             
 def find_sub_file(subid, filelist):
+    """
+    returns file of a specified subject contained in a list of files
+    """    
     subfile = [x for x in filelist if subnum in x]
     return subfile[0]    
 
 def load_mcpars(datadir, subid):
+    """
+    load 6 column motion correction parameter file into numpy array
+    """    
     subdir = os.path.join(datadir, '_'.join([subid, 'Preproc.feat']))
     mcpar_file = os.path.join(subdir, 'mc', 'prefiltered_func_data_mcf.par')
     mcpars = np.loadtxt(mcpar_file)
@@ -37,14 +43,19 @@ def main(icadir, datadir, tc_glob):
         subtc_dat, subtc_aff = utils.load_nii(sub_tc_file)
         sub_mcpars = load_mcpars(datadir, subid)
         sub_mcpars_full = gu.add_squares(sub_mcpars)
-    
+        betah, Yfitted, resid = gu.glm(sub_mcpars_full, subtc_dat)
+        subtc_cleaned = save_nii(subtc_file, resid, subtc_aff)
+        
 if __name__ == '__main__':
     
     ##### Set parameters ##########
-    icadir = '/home/jagust/rsfmri_ica/GIFT/GICA_d30'
-    datadir = '/home/jagust/rsfmri_ica/GIFT/data'
-    tc_glob = '*_sub*_timecourses_ica_s1_.nii'
+    icadir = '/home/jagust/rsfmri_ica/GIFT/GICA_d30' # Directory containing ica output
+    datadir = '/home/jagust/rsfmri_ica/GIFT/data' # Directory containing pre-processed data
+    tc_glob = '*_sub*_timecourses_ica_s1_.nii' # File name pattern to search for tc files
     ################################
+    
+    # Regress motion parameters out of component timecourses
+    main(icadir, datadir, tc_glob)
     
     
     
