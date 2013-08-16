a2cd5872c33f0cd6ad93543c52ed965fce0e532a
Jeremy Elman <jelman@lbl.gov>
2013-08-15 20:16:16 -0700

    Begin script to regress motion parameters out of GIFT timecourse nii files

---
+++ regress_motion.py
@@ -0,0 +1,10 @@
+import os, sys
+sys.path.insert(0, '/home/jagust/jelman/CODE/GIFT_analysis', '/home/jagust/jelman/CODE/misc')
+import general_utilities as utils
+import gift_utils as gu
+
+
+
+
+
+
